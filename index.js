1abc1aeaf6e9be2170cbd5e910243649a9b4065b
David Tsukanov <burtbukin1@gmail.com>
2025-12-25 19:27:22 +0300

    test

---
+++ index.js
@@ -0,0 +1,19 @@
+// Мини-задание с проверкой возраста
+const users = [
+    {name: "Alex", age: 16 },
+    {name: "Ivan", age: 21 }
+]
+
+const ageChecker = (user) => {
+    if (user.age >= 18) {
+        console.log(`${user.name}: Совершеннолетний`)
+    }
+
+    else {
+        console.log(`${user.name}: Несовершеннолетний`)
+    }
+}
+
+for (const user of users) {
+    ageChecker(user)
+}
